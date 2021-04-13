810cf6837bfadd95671b2ec9c023c3f69e4f34ef
Jzweifel15 <jzweifel15@yahoo.com>
2021-04-13 18:18:23 -0400

    Added Environment
    Created a virtual Python environment along with the main program file. The main program file, for now, consists of a function that asks the program User for input that stores the number of turtles to be raced

---
+++ turtle_racing.py
@@ -0,0 +1,23 @@
+import turtle
+
+# A function that asks the User for the number of turtles to race
+def get_number_of_racers():
+  racers = 0
+
+  while True:
+    racers = input("Enter the number of racers (2 - 10): ")
+
+    if racers.isdigit():
+      racers = int(racers)
+    else:
+      print("Input is not numeric... Try again!")
+      continue 
+
+    if 2 <= racers <= 10:
+      return racers
+    else:
+      print("Number not in range 2 - 10. Try again!")
+
+
+racers = get_number_of_racers()
+print(racers)
